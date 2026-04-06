a5d17b565904f639ed5805e0f7d31d208b4cc323
Vivek Kumar <vivek43nit@gmail.com>
2026-04-06 14:44:28 +0530

    Replace jdom2 with JDK built-in XML parser to eliminate CVE
    jdom2 2.0.6 has no patched version available. Replaced with javax.xml.parsers
    DocumentBuilder (JDK built-in, zero third-party risk) with XXE disabled.
    Removes the jdom2 dependency entirely.

--- src/main/java/com/vivek/sqlstorm/config/connection/parsers/DatabaseConfigXmlParser.java
+++ src/main/java/com/vivek/sqlstorm/config/connection/parsers/DatabaseConfigXmlParser.java
@@ -1,33 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.vivek.sqlstorm.config.connection.parsers;
 
 import com.vivek.sqlstorm.config.connection.ConnectionConfig;
 import com.vivek.sqlstorm.config.connection.ConnectionDTO;
 import com.vivek.utils.parser.ConfigParserInterface;
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.logging.Level;
 import org.apache.log4j.Logger;
-import org.jdom2.Document;
-import org.jdom2.Element;
-import org.jdom2.JDOMException;
-import org.jdom2.input.SAXBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
 
-/**
- *
- * @author root
- */
-public class DatabaseConfigXmlParser implements ConfigParserInterface<ConnectionConfig>{
-    
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DatabaseConfigXmlParser implements ConfigParserInterface<ConnectionConfig> {
+
     private static final Logger logger = Logger.getLogger(DatabaseConfigXmlParser.class);
-            
+
     @Override
     public String getApplicationName() {
         return "sql-storm";
@@ -41,62 +31,63 @@ public class DatabaseConfigXmlParser implements ConfigParserInterface<Connection
     @Override
     public ConnectionConfig parse(File file) {
         try {
-            SAXBuilder builder = new SAXBuilder();
-            // Disable XXE (XML External Entity) processing
-            builder.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
-            builder.setFeature("http://xml.org/sax/features/external-general-entities", false);
-            builder.setFeature("http://xml.org/sax/features/external-parameter-entities", false);
-            builder.setExpandEntities(false);
-            Document doc = builder.build(file);
-            Element root = doc.getRootElement();
-            
-            List connectionList = root.getChildren("CONNECTION");
-            List<ConnectionDTO> list = new ArrayList<ConnectionDTO>();
-            for(Iterator connectionIterator = connectionList.iterator(); connectionIterator.hasNext();)
-            {
-                Element connection = (Element)connectionIterator.next();
-                
-                String ID = connection.getAttributeValue("ID");
-                String DRIVER_CLASS_NAME = connection.getAttributeValue("DRIVER_CLASS_NAME");
-                String DATABASE_URL = connection.getAttributeValue("DATABASE_URL");
-                String USER_NAME = connection.getAttributeValue("USER_NAME");
-                String PASSWORD = connection.getAttributeValue("PASSWORD");
-                String group = connection.getAttributeValue("GROUP");
-                String DB_NAME = connection.getAttributeValue("DB_NAME");
-                
-                String UPDATABLE = connection.getAttributeValue("UPDATABLE");
-                String DELETABLE = connection.getAttributeValue("DELETABLE");
-                String searchableRowLimit = connection.getAttributeValue("NON_INDEXED_SEARCHABLE_ROW_LIMIT");
-                
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            // Disable XXE
+            factory.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
+            factory.setFeature("http://xml.org/sax/features/external-general-entities", false);
+            factory.setFeature("http://xml.org/sax/features/external-parameter-entities", false);
+            factory.setExpandEntityReferences(false);
+            factory.setXIncludeAware(false);
+
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document doc = builder.parse(file);
+            Element root = doc.getDocumentElement();
+
+            NodeList connectionList = root.getElementsByTagName("CONNECTION");
+            List<ConnectionDTO> list = new ArrayList<>();
+
+            for (int i = 0; i < connectionList.getLength(); i++) {
+                Element connection = (Element) connectionList.item(i);
+
+                String ID = connection.getAttribute("ID");
+                String DRIVER_CLASS_NAME = connection.getAttribute("DRIVER_CLASS_NAME");
+                String DATABASE_URL = connection.getAttribute("DATABASE_URL");
+                String USER_NAME = connection.getAttribute("USER_NAME");
+                String PASSWORD = connection.getAttribute("PASSWORD");
+                String group = connection.getAttribute("GROUP");
+                String DB_NAME = connection.getAttribute("DB_NAME");
+                String UPDATABLE = connection.getAttribute("UPDATABLE");
+                String DELETABLE = connection.getAttribute("DELETABLE");
+                String searchableRowLimit = connection.getAttribute("NON_INDEXED_SEARCHABLE_ROW_LIMIT");
+
                 ConnectionDTO connConfig = new ConnectionDTO(DRIVER_CLASS_NAME, DATABASE_URL, USER_NAME, PASSWORD, Long.parseLong(ID), group, DB_NAME);
-                if(UPDATABLE != null && !UPDATABLE.isEmpty() && !UPDATABLE.equals("null")){
+
+                if (UPDATABLE != null && !UPDATABLE.isEmpty() && !UPDATABLE.equals("null")) {
                     connConfig.setUpdatable(true);
                 }
-                if(DELETABLE != null && !DELETABLE.isEmpty() && !DELETABLE.equals("null")){
+                if (DELETABLE != null && !DELETABLE.isEmpty() && !DELETABLE.equals("null")) {
                     connConfig.setDeletable(true);
                 }
-                
-                if(searchableRowLimit != null && !searchableRowLimit.isEmpty()){
+                if (searchableRowLimit != null && !searchableRowLimit.isEmpty()) {
                     connConfig.setSearchableRowLimit(Integer.parseInt(searchableRowLimit));
                 }
-                
+
                 list.add(connConfig);
             }
+
             ConnectionConfig config = new ConnectionConfig(list);
-            String connectionExpiryTime = root.getAttributeValue("CONNECTION_EXPIRY_TIME");
-            String maxRetryCount = root.getAttributeValue("MAX_RETRY_COUNT");
-            
-            if(connectionExpiryTime != null && !connectionExpiryTime.isEmpty()){
+            String connectionExpiryTime = root.getAttribute("CONNECTION_EXPIRY_TIME");
+            String maxRetryCount = root.getAttribute("MAX_RETRY_COUNT");
+
+            if (connectionExpiryTime != null && !connectionExpiryTime.isEmpty()) {
                 config.setConnectionExpiryTime(Long.parseLong(connectionExpiryTime));
             }
-            if(maxRetryCount != null && !maxRetryCount.isEmpty()){
+            if (maxRetryCount != null && !maxRetryCount.isEmpty()) {
                 config.setMaxRetryCount(Integer.parseInt(maxRetryCount));
             }
+
             return config;
-        } catch (JDOMException ex) {
-            logger.error("Error in Parsing XML file", ex);
-            return null;
-        } catch (IOException ex) {
+        } catch (Exception ex) {
             logger.error("Error in Parsing XML file", ex);
             return null;
         }
